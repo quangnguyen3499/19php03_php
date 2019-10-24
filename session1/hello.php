c2f4ff9c5ecee6a1991b0ee7d2e2fdbd71bacbe8
Chad Luong <apple.luong1905@gmail.com>
2019-10-24 18:53:28 +0700

    update

--- session1/hello.php
+++ session1/hello.php
@@ -1,3 +1,9 @@
 <?php 
 	echo "<h1>Hello world!</h1>";
 ?>
+<h2>h2 tag</h2>
+<?php 
+	$userName = 'php03';
+	echo $userName;
+	echo "<br>";
+?>
---
+++ session2/handle_register.php
@@ -0,0 +1,5 @@
+<?php 
+	echo $_REQUEST['username']; // $_GET, $_POST
+	echo "<br>";
+	echo $_REQUEST['password'];
+?>
